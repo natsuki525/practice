e9be463a06caa1e6f8019902f7eead046be10fa9
natsuki525 <eteespoir@icloud.com>
2020-04-04 07:17:08 +0000

    [add] devise/controller&view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+  	sessions: 'devise/customers/sessions'
+  	passwords: 'devise/customers/passwords'
+  	registrations: 'devise/customers/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
