209a4fffc88827a361c4a6a0a31d098fa937d695
natsuki525 <eteespoir@icloud.com>
2020-04-04 08:42:55 +0000

    [fix] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers, controllers: {
-  	sessions: 'devise/customers/sessions'
-  	passwords: 'devise/customers/passwords'
+  	sessions: 'devise/customers/sessions',
+  	passwords: 'devise/customers/passwords',
   	registrations: 'devise/customers/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
