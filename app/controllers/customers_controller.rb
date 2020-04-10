fe66aca0f4ebf1f04f7eef1cd9bd63e325f3c3ca
natsuki525 <eteespoir@icloud.com>
2020-04-10 14:57:46 +0000

    [add]customer/show&edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,4 +3,16 @@ class CustomersController < ApplicationController
 	def show
 		@customer = current_customer
 	end
+	def edit
+		@customer = current_customer
+	end
+	def update
+		@customer = current_customer
+		if @customer.update(customer_params)
+			redirect customer_path
+			flash[:notice_update] = "会員情報が更新されました！"
+		else
+			render 'edit'
+		end
+	end
 end
