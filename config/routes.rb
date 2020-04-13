222002ef3cefbe732e7921dc97a2ddea55056c07
natsuki525 <eteespoir@icloud.com>
2020-04-13 15:06:13 +0000

    [add] admin/genreルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
