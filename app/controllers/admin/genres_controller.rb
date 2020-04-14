e96a92ea7ef61248b33adc6960beb814db2a7cbc
natsuki525 <eteespoir@icloud.com>
2020-04-14 06:26:31 +0000

    [add] admin/genre編集画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -14,6 +14,18 @@ class Admin::GenresController < ApplicationController
 			render 'index'
 		end
 	end
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+	def update
+		@genre = Genre.find(params[:id])
+		if @genre.update(genre_params)
+			redirect_to admin_genres_path
+			flash[:notice_update] = 'ジャンルを更新しました！'
+		else
+			reder 'edit'
+		end
+	end
 	private
 	def genre_params
 		params.require(:genre).permit(:name,:is_active)
