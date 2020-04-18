b856de24b53e4750e7a65a8c35b754abb2829420
natsuki525 <eteespoir@icloud.com>
2020-04-18 07:39:41 +0000

    [add] アソシエーション記述

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,5 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :shippings
+  has_many :cart_items
 end
